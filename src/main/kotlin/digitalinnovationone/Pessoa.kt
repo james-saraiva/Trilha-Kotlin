2df24825251905c1ae37cc126fae8ca2e6ca802b
james-saraiva <james.saraiva@etec.sp.gov.br>
2021-10-11 21:58:36 -0300

    Adicionada uma classe interna chamada endereco. Também foi efetuada a privatização da classe pessoa, conferindo que não haverão informações alteradas dentro dos atributos desta classe.

--- src/main/kotlin/digitalinnovationone/Pessoa.kt
+++ src/main/kotlin/digitalinnovationone/Pessoa.kt
@@ -3,12 +3,18 @@ package digitalinnovationone
 class Pessoa {
     var nome: String = "James"
     var cpf: String = "123.456.789.10"
+    private set //Priva qualquer mudança de dados enquanto o código é executado apenas para a classe pessoa
 
+//    inner class Endereco{
+//        var tipoend: String = "rua" //Esta é uma classe interna da classe Pessoa
+//    }
 }
 
 fun main(){
-    val james = Pessoa()
+    val pessoa = Pessoa()
+    println(pessoa.nome)
+    println(pessoa.cpf)
 
-    println(james.nome)
-    println(james.cpf)
+//    james.cpf = "1234564" //não pode ser executado pois está tentando modificar um dado de um atributo da classe pessoa
+//    println(james.Endereco().tipoend) //caminho para acessar um atributo de uma classe interna
 }
